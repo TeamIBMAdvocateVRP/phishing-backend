602ff6524836336d7eca65b56df83175bb4ebbd2
malidevf <mali@devf.mx>
2020-08-25 13:26:44 -0500

    add env port

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const parser = require('body-parser');
 require('dotenv').config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const options = {
     useNewUrlParser: true,
